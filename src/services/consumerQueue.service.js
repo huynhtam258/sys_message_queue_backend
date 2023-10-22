9a78c97647698c08a986c8d2f370ea8fa8e65f27
Huynh Manh Tam <huynhtam258@gmail.com>
2023-10-22 15:49:54 +0700

    feat: Handle logic case message queue

--- src/services/consumerQueue.service.js
+++ src/services/consumerQueue.service.js
@@ -20,15 +20,40 @@ const messageService = {
             const { channel, connection } = await connectToRabbitMQ()
             const notiQueue = 'notificationQueueProcess' // assertQueue
 
+            // 1. TTL
             // timeExpried: thời gian nhận tin nhắn
             // const timeExpried = 5000 // trường hợp thành công
-            const timeExpried = 15000 // trường hợp test thất bại
-            setTimeout(() => {
-                channel.consume(notiQueue, msg => {
-                    console.log(`SEND notificationQueue successfully processed::`, msg.content.toString());
+            // const timeExpried = 15000 // trường hợp test thất bại
+            // setTimeout(() => {
+            //     channel.consume(notiQueue, msg => {
+            //         console.log(`SEND notificationQueue successfully processed::`, msg.content.toString());
+            //         channel.ack(msg)
+            //     }) 
+            // }, timeExpried);
+
+            // 2. LOGIC
+            channel.consume(notiQueue, msg => {
+                try {
+                    const numberTest = Math.random()
+                    console.log({ numberTest})
+                    if (numberTest < 0.8) {
+                        throw new Error('Send notification failed: HOT FIX')
+                    }
+
+                    console.log('SEND notification Queue successfully processed:', msg.content.toString())
                     channel.ack(msg)
-                }) 
-            }, timeExpried);
+                } catch (error) {
+                    // console.error('SEND notification error:', error);
+                    channel.nack(msg, false, false)
+                    // nack: khi dữ liệu bị lỗi, msg sẽ được ném vào queue lỗi
+                    /*
+                        nack: negative acknowledgement
+                        params 1: tin nhắn
+                        params 2: có nên sắp xếp lại tin nhắn hay không, nếu là true sẽ đẩy lại ví trí ban đầu, false thì đẩy tiếp vào hàng đợi bị lỗi
+                        params 3: có muốn từ chối nhiều thư hay không
+                    */
+                }
+            })
 
         } catch (error) {
             console.error(error)
